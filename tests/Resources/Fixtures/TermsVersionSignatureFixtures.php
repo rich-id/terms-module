4a6c759dfaba4066126e6971d5cb578e4d8f9787
Dumazeau <hdumazeau@richcongress.com>
2021-07-22 08:53:53 +0200

    Clean code + add new route

--- tests/Resources/Fixtures/TermsVersionSignatureFixtures.php
+++ tests/Resources/Fixtures/TermsVersionSignatureFixtures.php
@@ -16,6 +16,7 @@ final class TermsVersionSignatureFixtures extends AbstractFixture implements Dep
         $termsVersion1 = $this->getReference(TermsVersion::class, 'v1-terms-1');
         $termsVersion2 = $this->getReference(TermsVersion::class, 'v2-terms-1');
         $termsVersion3 = $this->getReference(TermsVersion::class, 'v3-terms-1');
+        $termsVersion4 = $this->getReference(TermsVersion::class, 'v1-terms-5');
 
         $this->createObject(
             TermsVersionSignature::class,
@@ -69,6 +70,17 @@ final class TermsVersionSignatureFixtures extends AbstractFixture implements Dep
                 'subjectType'       => 'user',
                 'subjectIdentifier' => '43',
                 'version'           => $termsVersion3,
+            ]
+        );
+
+        $this->createObject(
+            TermsVersionSignature::class,
+            'my_user_2-signature-v1-terms-5',
+            [
+                'date'              => new \DateTime(),
+                'subjectType'       => 'user',
+                'subjectIdentifier' => 'my_user_2',
+                'version'           => $termsVersion4,
             ]
         );
     }
