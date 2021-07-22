af640406f3ae3693dfb29fc77141f17da240cd5e
Dumazeau <hdumazeau@richcongress.com>
2021-07-22 09:24:18 +0200

    Add new events

---
+++ src/Domain/Event/TermsPublishedEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RichId\TermsModuleBundle\Domain\Event;
+
+use RichId\TermsModuleBundle\Domain\Entity\Terms;
+
+final class TermsPublishedEvent extends TermsEvent
+{
+    /** @var Terms */
+    private $terms;
+
+    public function __construct(Terms $terms)
+    {
+        $this->terms = $terms;
+    }
+
+    public function getTerms(): Terms
+    {
+        return $this->terms;
+    }
+}
