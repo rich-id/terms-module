52f3aeebbcd63e02a0492cc42c02e48f0d1ed097
Dumazeau <hdumazeau@richcongress.com>
2023-04-26 12:05:49 +0200

    Clean code, add voter

--- src/UserInterface/Controller/AdminRoute.php
+++ src/UserInterface/Controller/AdminRoute.php
@@ -13,6 +13,6 @@ class AdminRoute extends AbstractController
     /** @IsGranted("MODULE_TERMS_ADMIN") */
     public function __invoke(): Response
     {
-        return $this->render('@RichIdTermsModule/admin/main.html.twig',);
+        return $this->render('@RichIdTermsModule/admin/main.html.twig', );
     }
 }
---
+++ tests/UserInterface/Controller/AdminRouteTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RichId\TermsModuleBundle\Tests\UserInterface\Controller;
+
+use RichCongress\TestFramework\TestConfiguration\Annotation\TestConfig;
+use RichCongress\TestSuite\TestCase\ControllerTestCase;
+use RichId\TermsModuleBundle\Tests\Resources\Entity\DummyUser;
+use RichId\TermsModuleBundle\Tests\Resources\Fixtures\DummyUserFixtures;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @covers \RichId\TermsModuleBundle\UserInterface\Controller\AdminRoute
+ * @TestConfig("fixtures")
+ */
+final class AdminRouteTest extends ControllerTestCase
+{
+    public function testRouteNotLogged(): void
+    {
+        $response = $this->getClient()->get('/administration/terms/home');
+        $this->assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testRouteBadRole(): void
+    {
+        $this->authenticate(DummyUser::class, DummyUserFixtures::USER);
+
+        $response = $this->getClient()->get('/administration/terms/home');
+        $this->assertSame(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+    }
+
+    public function testRouteAsAdmin(): void
+    {
+        $this->authenticate(DummyUser::class, DummyUserFixtures::USER_ADMIN);
+
+        $response = $this->getClient()->get('/administration/terms/home');
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $content = $response->getContent() !== false ? $response->getContent() : '';
+
+        $this->assertStringContainsString('Terms management', $content);
+        $this->assertStringContainsString('Terms list', $content);
+    }
+}
